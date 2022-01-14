dd714f69d82e57bdaf612385221c4bc87451c44b
khulanJargalsaikhan <onagraa@gmail.com>
2022-01-14 12:45:31 -0500

    two sum

--- src/Array/PlusOne.java
+++ src/Array/PlusOne.java
@@ -1,5 +1,6 @@
 package Array;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PlusOne {
@@ -8,6 +9,7 @@ public class PlusOne {
 		System.out.println(Arrays.toString(plusOne(new int[] {1,2,3})));
 		System.out.println(Arrays.toString(plusOne(new int[] {4,3,2,1})));
 		System.out.println(Arrays.toString(plusOne(new int[] {9})));
+		System.out.println(Arrays.toString(plusOne(new int[] {9,8,7,6,5,4,3,2,1,0})));
 
 	}
 
@@ -33,33 +35,62 @@ Incrementing by one gives 4321 + 1 = 4322.
 Thus, the result should be [4,3,2,2].
 	 */
 
-	public static int[] plusOne(int[] digits) {
-		
+	public static int[] plusOne1(int[] digits) {
+
 		StringBuilder sb = new StringBuilder();
-		
+
 		for (int digit : digits) {
 			sb.append(digit);
 		}
-		
+
 		int sbInt = Integer.parseInt(sb.toString());
 		int total = sbInt + 1;
-		
+
 		String totalString = String.valueOf(total);
-		
-		
+
+
 		int[] arr = new int[totalString.length()];
-		
+
 		for (int i=0; i<totalString.length(); i++) {
 			arr[i] = Integer.parseInt(totalString.substring(i,i+1));
 		}
-		
-		
+
+
 		return arr;
 	}
-	
-	
-	
-	
-	
+
+	public static int[] plusOne(int[] digits) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		if (digits == null || digits.length == 0) {
+			int[] temp = {1};
+			return temp;
+		}
+
+		int carry = 0;
+		for (int i = digits.length - 1; i >= 0; i--) {
+			if (i == digits.length - 1) {
+				carry = carry + digits[i] + 1;
+			} else {
+				carry += digits[i];
+			}
+			result.add(0, carry % 10);
+			carry /= 10;
+		}
+
+		if (carry == 1) {
+			result.add(0, 1);
+		}
+		int resultArray[] = new int[result.size()];
+		for (int i = 0; i < result.size(); i++) {
+			resultArray[i] = result.get(i);
+		}
+
+		return resultArray;
+	}
+
+
+
+
+
 
 }
