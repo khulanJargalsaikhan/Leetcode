879ae4319f47c2acc3db89fe3910008ec2297d24
khulanJargalsaikhan <onagraa@gmail.com>
2022-01-25 14:26:32 -0500

    Merge Two Lists - recursive solution

--- src/LinkedList/MergeTwoSortedLists.java
+++ src/LinkedList/MergeTwoSortedLists.java
@@ -35,8 +35,27 @@ public class MergeTwoSortedLists {
 
 		return dummyHead.next;
 	}
-	
-	
+
+
 	//Recursive Solution
-	
+	public static ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+
+		if (list1 == null)
+			return list2;
+
+		if (list2 == null)
+			return list1;
+
+
+		if (list1.val < list2.val){
+			list1.next = mergeTwoLists2(list1.next, list2);
+			return list1;
+		}else {
+			list2.next = mergeTwoLists2(list2.next, list1);
+			return list2;
+		}     
+
+	}
+
+
 }
