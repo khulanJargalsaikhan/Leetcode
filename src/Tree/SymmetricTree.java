762fd608a7cb40a426a95481edd895caea61455a
khulanJargalsaikhan <onagraa@gmail.com>
2022-02-01 18:59:06 -0500

    Symmetric tree

---
+++ src/Tree/SymmetricTree.java
@@ -0,0 +1,43 @@
+package Tree;
+
+import Tree.MaximumDepthOfBinaryTree.TreeNode;
+
+public class SymmetricTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	/*
+	 * Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+	 */
+
+	public boolean isSymmetric(TreeNode root) {
+
+		// if root is empty, then technically it is symmetric
+		if (root == null) return true;
+
+		// recursive solution
+		return isSymmetric(root.left, root.right);
+	}
+
+	boolean isSymmetric(TreeNode left, TreeNode right){
+		
+		//if left or right is null, then let's check left and right are equal
+		if (left == null || right == null){
+			return left == right; 
+		}
+
+		if (left.val != right.val){
+			return false;
+		}
+		
+		return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+	}
+
+}
