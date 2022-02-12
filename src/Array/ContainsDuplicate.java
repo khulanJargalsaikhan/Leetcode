c782a35c1152a626e3f6d3a903b02ae30db793bc
khulanJargalsaikhan <onagraa@gmail.com>
2022-02-12 16:38:16 -0500

    enhanced Contains Duplicate problem

--- src/Array/ContainsDuplicate.java
+++ src/Array/ContainsDuplicate.java
@@ -1,45 +1,45 @@
 package Array;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
 
 	public static void main(String[] args) {
 
 
-		System.out.println(Solution11.containsDuplicate(new int[] {1,2,3,1}));	//true
-		System.out.println(Solution11.containsDuplicate(new int[] {1,2,3,4}));	//false
-		System.out.println(Solution11.containsDuplicate(new int[] {1,1,1,3,3,4,3,2,4,2})); // true
+		System.out.println(containsDuplicate2(new int[] {1,2,3,1}));	//true
+		System.out.println(containsDuplicate2(new int[] {1,2,3,4}));	//false
+		System.out.println(containsDuplicate2(new int[] {1,1,1,3,3,4,3,2,4,2})); // true
 
 	}
-
-
-
-}
-
-
-
-class Solution11 {
+	/*
+	 * Given an integer array nums, return true if any value appears at least twice in the array, 
+	 * and return false if every element is distinct.
+	 	Input: nums = [1,2,3,1]
+		Output: true
+		Input: nums = [1,2,3,4]
+		Output: false
+	 */
 	public static boolean containsDuplicate(int[] nums) {
-		/*
-		 * Given an integer array nums, return true if any value appears at least twice in the array, 
-		 * and return false if every element is distinct.
-		 * 
-		 	Input: nums = [1,2,3,1]
-			Output: true
-
-			Input: nums = [1,2,3,4]
-			Output: false
-		 */
-
 		for (int i=0; i<nums.length-1; i++){  
-
 			for (int j=i+1; j<nums.length; j++) {
 				if (nums[i] == nums[j])
 					return true;
 			}
-
 		}
-
 		return false;
-
+	}
+	
+	public static boolean containsDuplicate2(int[] nums) {
+		Set<Integer> myset = new HashSet<>();
+		
+		for (int i=0; i<nums.length; i++){  
+			if (!myset.contains(nums[i]))
+				myset.add(nums[i]);
+			else
+				return true;
+		}
+		return false;
 	}
 }
