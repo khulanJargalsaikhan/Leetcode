6307834dfa69ac0c4359cf153e6efbf7eb6a89d4
khulanJargalsaikhan <onagraa@gmail.com>
2022-06-01 21:22:53 -0400

    better solution

--- src/Array/TwoSum.java
+++ src/Array/TwoSum.java
@@ -1,6 +1,8 @@
 package Array;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
 
@@ -14,6 +16,11 @@ public class TwoSum {
 		//		List<String> words = new ArrayList<String>(Arrays.asList("Hello", "World"));
 		//		words.remove("Hello");
 		//		System.out.println(words);
+		
+		System.out.println(Arrays.toString(twoSum2(new int[] {5, 3, 5, 7}, 10))); //0,2  1,3
+		System.out.println(Arrays.toString(twoSum2(new int[] {3, 2, 4}, 6)));  //1,2
+		System.out.println(Arrays.toString(twoSum2(new int[] {2, 7, 11, 15}, 9)));  //0,1
+
 
 	}
 
@@ -47,5 +54,23 @@ public class TwoSum {
 			}
 		}
 		return result;
+	}
+
+
+	public static int[] twoSum2(int[] nums, int target) {
+		Map<Integer, Integer> mymap = new HashMap<>();
+		for (int i=0; i<nums.length; i++) {
+			mymap.put(nums[i], i);
+		}
+		
+		for (int i=0; i<nums.length; i++) {
+			int res = target - nums[i];
+			if (mymap.containsKey(res) && mymap.get(res) != i) {
+				return new int[] {i, mymap.get(res)};
+			}
+		}
+		
+		return null;
+		
 	}
 }
