e39fb4c8feb7a20cbd6d6b94d4afdf598c58b5c3
khulanJargalsaikhan <onagraa@gmail.com>
2022-06-27 19:29:29 -0400

    better solution - HashMap is used

--- src/HashTable/HowManyNumbersAreSmallerThantheCurrentNumber.java
+++ src/HashTable/HowManyNumbersAreSmallerThantheCurrentNumber.java
@@ -1,10 +1,13 @@
 package HashTable;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HowManyNumbersAreSmallerThantheCurrentNumber {
     public static void main(String[] arg){
-        System.out.println(Arrays.toString(smallerNumbersThanCurrent(new int[] {8,1,2,2,3})));
+        //System.out.println(Arrays.toString(smallerNumbersThanCurrent(new int[] {8,1,2,2,3})));
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent2(new int[] {8,1,2,2,3})));
     }
     /*
 Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
@@ -30,11 +33,32 @@ Output: [4,0,1,1,3]
             result[j] = count;
             j++;
         }
-
-
         return result;
     }
 
+    public static int[] smallerNumbersThanCurrent2(int[] nums) {
+        int[] result = new int[nums.length];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        //keep the original array for key of the map
+        int[] original = nums.clone();
+
+        //sort the array
+        Arrays.sort(nums);
+
+        //use sorted array for value of the map
+        for (int i=0; i<nums.length; i++){
+            if (!map.containsKey(nums[i])){
+                map.put(nums[i], i);
+            }
+        }
+
+        //get the values (index numbers) associated with key (original array)
+        for (int i=0; i<nums.length; i++){
+            result[i] = map.get(original[i]);
+        }
+        return result;
+    }
 }
 
 
