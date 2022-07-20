4cca8780681b0edc091ef4d29e09283279d608cc
khulanJargalsaikhan <onagraa@gmail.com>
2022-07-20 15:25:17 -0400

    Hashtable - better solution

--- src/HashTable/KthDistinctStringInanArray.java
+++ src/HashTable/KthDistinctStringInanArray.java
@@ -21,10 +21,15 @@ public class KthDistinctStringInanArray {
         System.out.println(kthDistinct(new String[] {"d","b","c","b","c","a"}, 2));  //a
         System.out.println(kthDistinct(new String[] {"aaa","aa","a"}, 1));   //aaa
         System.out.println(kthDistinct(new String[] {"a","b","a"}, 3));   //""
+
+        System.out.println(kthDistinct2(new String[] {"d","b","c","b","c","a"}, 2));  //a
+        System.out.println(kthDistinct2(new String[] {"aaa","aa","a"}, 1));   //aaa
+        System.out.println(kthDistinct2(new String[] {"a","b","a"}, 3));   //""
     }
 
     public static String kthDistinct(String[] arr, int k) {
         //create map and save input string as keys, frequency of the string as values
+        //LinkedHashMap maintains order of input
         Map<String, Integer> map = new LinkedHashMap<>();
         for (String str : arr){
             map.put(str, map.getOrDefault(str, 0) + 1);
@@ -43,6 +48,21 @@ public class KthDistinctStringInanArray {
             return "";
         }
 
+    }
+
+    //faster solution
+    public static String kthDistinct2(String[] arr, int k) {
+        //create map and save input string as keys, frequency of the string as values
+        Map<String, Integer> map = new HashMap<>();
+        //filter the arr and copy only unique str into the map
+        Arrays.stream(arr).forEach(x -> map.put(x, map.getOrDefault(x, 0)+1));
+        //loop through input arr, and if str from array as map key matches with map value, then reduce k by 1
+        for (String str : arr){
+            if (map.get(str) == 1 && --k == 0){
+                return str;
+            }
+        }
+        return "";
     }
 
 }
