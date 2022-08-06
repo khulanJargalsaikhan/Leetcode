fc254c5448b00453e6210e8ec453539df5fa2cbd
khulanJargalsaikhan <onagraa@gmail.com>
2022-08-06 16:43:43 -0400

    two pointers - array - better solution

--- src/TwoPointers/IntersectionOfTwoArrays.java
+++ src/TwoPointers/IntersectionOfTwoArrays.java
@@ -6,8 +6,11 @@ import java.util.Set;
 
 public class IntersectionOfTwoArrays {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(intersection(new int[] {1,2,2,1}, new int[] {2,2})));
-        System.out.println(Arrays.toString(intersection(new int[] {4,5,9}, new int[] {9,4,9,8,4})));
+        System.out.println(Arrays.toString(intersection(new int[] {1,2,2,1}, new int[] {2,2}))); //[2]
+        System.out.println(Arrays.toString(intersection(new int[] {4,5,9}, new int[] {9,4,9,8,4}))); //[4,9]
+
+        System.out.println(Arrays.toString(intersection2(new int[] {1,2,2,1}, new int[] {2,2}))); //[2]
+        System.out.println(Arrays.toString(intersection2(new int[] {4,5,9}, new int[] {9,4,9,8,4}))); //[4,9]
 
     }
     //brute force approach
@@ -25,6 +28,27 @@ public class IntersectionOfTwoArrays {
         for (int num : set){
             result[i++] = num;
         }
+        return result;
+    }
+
+/*
+Complexity Analysis
+Time complexity : O(n+m) in the average case and O(n*m) in the worst case when load factor is high enough.
+Space complexity : O(n+m) in the worst case when all elements in the arrays are different.
+*/
+    public static int[] intersection2(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        //converting nums1, nums2 arrays to set1 and set2
+        for (int num : nums1) set1.add(num);
+        for (int num : nums2) set2.add(num);
+        //set1 keeps only matching elements of set2
+        set1.retainAll(set2);
+        //copy set1 to result array
+        int[] result = new int[set1.size()];
+        int i = 0;
+        for (Integer num : set1) result[i++] = num;
+
         return result;
     }
 }
