6b4e6e7e92c0ae6b5c2f50cda81e5a6cb279fded
khulanJargalsaikhan <onagraa@gmail.com>
2022-10-07 16:00:23 -0400

    better solution for SortArrayByParityII

--- src/Array/SortArrayByParityII.java
+++ src/Array/SortArrayByParityII.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 public class SortArrayByParityII {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(sortArrayByParityII(new int[] {4,2,5,7})));  //[4,5,2,7]
+        System.out.println(Arrays.toString(sortArrayByParityII2(new int[] {4,2,5,7})));  //[4,5,2,7]
+        System.out.println(Arrays.toString(sortArrayByParityII2(new int[] {888,505,627,846})));  //[888,505,846,627]
+
     }
     /*
 Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
@@ -35,4 +38,26 @@ Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
 
         return ans;
     }
+
+    //solution #2 : sort them in place
+    public static int[] sortArrayByParityII2(int[] nums) {
+        //put all even elements in the correct place, since all odd elements
+        //will be in the correct place too. So let's only focus on A[0], A[2], A[4], ...
+        int j=1;
+        for (int i=0; i<nums.length; i+=2){
+            if (nums[i] % 2 == 1) {
+                //if odd elements in the correct place, then go and check the next one
+                while (nums[j] % 2 == 1)
+                    j+=2;
+
+                //swap here
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+
+        return nums;
+    }
+
 }
